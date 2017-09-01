a4d89c72bd355252d4c5a8835991187b347ddec3
ITA student <student@itas-mac-mini-2.local>
2017-09-01 16:37:54 +0300

    Initial Commit

---
+++ toDoList Console/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  toDoList Console
+//
+//  Created by ITA student on 9/1/17.
+//  Copyright © 2017 Yurii Kupa. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
